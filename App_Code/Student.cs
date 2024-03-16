3b7b7a60b6ff09d3794766d903656ccd9b2e67ce
zzzYbing <152384657+zzzYbing@users.noreply.github.com>
2024-03-16 16:38:46 +0800

    Add files via upload

---
+++ App_Code/Student.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace StudentMmt.Models
+{
+    public class Student
+    {
+        public string StudentID { get; set; }
+        public string Name { get; set; }
+        public string Major { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Address { get; set; }
+        public int a { get; set; }
+        public int b { get; set; }
+        public int c { get; set; }
+        public int d { get; set; }
+        public int All { get; set; }
+    }
+}
